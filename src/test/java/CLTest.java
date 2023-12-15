ea3229b7dc691463f47f5a7bed404c4f3076f027
Rassene M'sadaa <rassens.msadaa@gmail.com>
2023-12-15 16:18:35 +0000

    more tests for protect

--- src/test/java/CLTest.java
+++ src/test/java/CLTest.java
@@ -1,53 +1,108 @@
-import example.CL;
+import com.google.gson.JsonObject;
+import proj.CL;
 import org.junit.jupiter.api.Assertions;
 
+import javax.crypto.spec.SecretKeySpec;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.security.Key;
+import java.security.SecureRandom;
+import java.util.Base64;
 
 class CLTest extends CL {
 
     @org.junit.jupiter.api.Test
-    void testGenerateAESKey() throws GeneralSecurityException, IOException {
-        CL.generateAESKey("Keys/AESKeytest.key");
-    }
-
-    @org.junit.jupiter.api.Test
-    void testReadAESKey() throws GeneralSecurityException, IOException {
+    void testGenerateAndReadAESKey() throws GeneralSecurityException, IOException {
         Key r = CL.generateAESKey("Keys/AESKeytest.key");
         Key t = CL.readAESKey("Keys/AESKeytest.key");
         Assertions.assertEquals(r, t);
     }
 
     @org.junit.jupiter.api.Test
-    void testCalculateMAC() {
+    void testProtectUnprotectFirst() throws IOException, GeneralSecurityException {
+        SecureRandom random = new SecureRandom();
+        byte[] nonce = new byte[16];
+        random.nextBytes(nonce);
+
+        String message = "Hello world!";
+        int id = 1;
+        CL.generateAESKey("Keys/AESKeytest.key");
+        Key key = CL.readAESKey("Keys/AESKeytest.key");
+
+        JsonObject r = CL.protect(message, nonce, id, key);
+
+        JsonObject     t = CL.unprotect(r, key);
+        String M = t.get("M").getAsString();
+
+        Assertions.assertEquals(message, M);
     }
 
     @org.junit.jupiter.api.Test
-    void testCalculateMAC1() {
+    void testProtectUnprotectSecond() throws IOException, GeneralSecurityException {
+        SecureRandom random = new SecureRandom();
+        byte[] nonce = new byte[16];
+        random.nextBytes(nonce);
+
+        String message = "Hello world!";
+        int id = 1;
+        CL.generateAESKey("Keys/AESKeytest.key");
+        CL.generateAESKey("Keys/AESFamilyKeytest.key");
+        Key key = CL.readAESKey("Keys/AESKeytest.key");
+        Key key_f = CL.readAESKey("Keys/AESFamilyKeytest.key");
+
+
+        JsonObject r = CL.protect(message, nonce, key, key_f);
+
+        JsonObject     t = CL.unprotect("CBC", r, key, nonce);
+        String M = t.get("M").getAsString();
+        Key k_f = new SecretKeySpec(Base64.getDecoder().decode(t.get("Key_f").getAsString()), "AES");
+
+
+        Assertions.assertEquals(message, M);
+        Assertions.assertEquals(key_f, k_f);
+
+    }
+    @org.junit.jupiter.api.Test
+    void testProtectUnprotectThird() throws IOException, GeneralSecurityException {
+        SecureRandom random = new SecureRandom();
+        byte[] nonce = new byte[16];
+        random.nextBytes(nonce);
+
+        String message = "Hello world!";
+        int id = 1;
+        CL.generateAESKey("Keys/AESKeytest.key");
+        Key key = CL.readAESKey("Keys/AESKeytest.key");
+
+
+        JsonObject r = CL.protect("CBC", message, nonce, key);
+
+        JsonObject     t = CL.unprotect("CBC", r, key, nonce);
+        String M = t.get("M").getAsString();
+
+
+        Assertions.assertEquals(message, M);
+
     }
 
     @org.junit.jupiter.api.Test
-    void testConcatenateByteArrays() {
-    }
+    void testProtectUnprotectFourth() throws IOException, GeneralSecurityException {
+        SecureRandom random = new SecureRandom();
+        byte[] nonce = new byte[16];
+        random.nextBytes(nonce);
 
-    @org.junit.jupiter.api.Test
-    void testProtect() {
-    }
+        String message = "Hello world!";
+        int id = 1;
+        CL.generateAESKey("Keys/AESKeytest.key");
+        Key key = CL.readAESKey("Keys/AESKeytest.key");
 
-    @org.junit.jupiter.api.Test
-    void testProtect1() {
-    }
 
-    @org.junit.jupiter.api.Test
-    void testProtect2() {
-    }
+        JsonObject r = CL.protect("CBC", message, nonce, key);
 
-    @org.junit.jupiter.api.Test
-    void testUnprotect() {
-    }
+        JsonObject     t = CL.unprotect("CBC", r, key, nonce);
+        String M = t.get("M").getAsString();
 
-    @org.junit.jupiter.api.Test
-    void testUnprotect1() {
+
+        Assertions.assertEquals(message, M);
+
     }
 }
