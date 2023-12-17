3d7ddba61ca7e03eb8b76ea64ad10b8a3efd0d84
Cherie000 <93259744+Cherie000@users.noreply.github.com>
2023-12-17 14:08:28 +0000

    Update Client.java

--- src/main/java/proj/Client.java
+++ src/main/java/proj/Client.java
@@ -1,15 +1,26 @@
 package proj;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.security.GeneralSecurityException;
+import java.security.Key;
+import java.security.SecureRandom;
+import java.util.Base64;
 import java.util.Scanner;
 
 import javax.net.SocketFactory;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
+
+import static proj.CL.*;
+
 
 public class Client {
 
@@ -31,31 +42,110 @@ public class Client {
             byte[] data = new byte[2048];
             int len = is.read(data);
             System.out.printf("client received %d bytes: %s%n", len, new String(data, 0, len));
-
-            String line = "";
+            
             try (Scanner scanner = new Scanner(System.in)) {
                 System.out.println("Type: 'Exit' to close the connection");
 
-                while (!line.equals("Exit")) {
                     try {
-                        line = scanner.nextLine();
-                        ;
-                        System.out.println("sending message: " + line);
+                        SecureRandom random = new SecureRandom();
+                        byte[] nonce = new byte[16];
+                        random.nextBytes(nonce);
+                        int id = 1;
+                        Key key = CL.readAESKey("Keys/AESKeyClient.key");
+                        Key key_f = CL.readAESKey("Keys/AESFamilyKeyClient.key");
+
+                        //First Message
+                        message = "This Song";
+                        JsonObject r = CL.protect(message, nonce, id, key);
+                        byte[] messageBytes = r.toString().getBytes();
                         os = new BufferedOutputStream(socket.getOutputStream());
-                        os.write(line.getBytes());
+                        os.write(messageBytes);
                         os.flush();
 
+                        //Listen for Response
                         is = new BufferedInputStream(socket.getInputStream());
                         data = new byte[2048];
                         len = is.read(data);
-                        System.out.printf("client received %d bytes: %s%n", len, new String(data, 0, len));
+                        if (len != -1) {
+                            String firstMessage = new String(data, 0, len);
+                            JsonObject receivedJson1 = JsonParser.parseString(firstMessage).getAsJsonObject();
+                            JsonObject decryptedJson1 = unprotect("CBC", receivedJson1, key, nonce);
+                            if(check(decryptedJson1.get("M").getAsString(), nonce,key,receivedJson1.get("MAC").getAsString())){
+                                // Check if the message is an error message
+                                String m = decryptedJson1.get("M").getAsString();
+                                if (m.equalsIgnoreCase("error")) {
+                                    System.out.println("Error message received. Aborting communication.");
+                                    // Close the connection
+                                    socket.close();
+                                    return;
+                                } else {
+                                    System.out.printf("Client received %d bytes: %s%n", len, message);
+                                    // Continue to send the second message
+                                }
+                            }else {
+                                System.out.println("Integrity check failed. Sending error message to server.");
+                                // Send error message to server
+                                String errorMessage = "Error";
+                                nonce = incrementByteNonce(nonce);
+                                r = CL.protect(message, nonce, key, key_f);
+                                messageBytes = r.toString().getBytes();
+                                os.write(messageBytes);
+                                os.flush();
+                                // Close the connection
+                                socket.close();
+                                return;
+                            }
+                        }
+                        // Second message
+                        message = "These Bytes";
+                        nonce = incrementByteNonce(nonce);
+                        r = CL.protect(message, nonce, key, key_f);
+                        messageBytes = r.toString().getBytes();
+                        os.write(messageBytes);
+                        os.flush();
+
+                        //Listen for Response
+                        len = is.read(data);
+                        if (len != -1) {
+                            String secondMessage = new String(data, 0, len);
+                            JsonObject receivedJson2 = JsonParser.parseString(secondMessage).getAsJsonObject();
+                            JsonObject decryptedJson2 = unprotect("CTR", receivedJson2, key, nonce);
+                            if(check(decryptedJson2.get("M").getAsString(), nonce,key,receivedJson2.get("MAC").getAsString())){
+                                // Check if the message is an error message
+                                String m = decryptedJson2.get("M").getAsString();
+                                if (m.equalsIgnoreCase("error")) {
+                                    System.out.println("Error message received. Aborting communication.");
+                                    // Close the connection
+                                    socket.close();
+                                    return;
+                                } else {
+                                    System.out.printf("Client received %d bytes: %s%n", len, message);
+                                }
+                            }else {
+                                System.out.println("Integrity check failed. Sending error message to server.");
+                                // Send error message to server
+                                String errorMessage = "Error";
+                                nonce = incrementByteNonce(nonce);
+                                r = CL.protect(message, nonce, key, key_f);
+                                messageBytes = r.toString().getBytes();
+                                os.write(messageBytes);
+                                os.flush();
+                                // Close the connection
+                                socket.close();
+                                return;
+                            }
+                        }
+
+
                     } catch (IOException i) {
                         System.out.println(i);
                         return;
+                    } catch (GeneralSecurityException e) {
+                        throw new RuntimeException(e);
                     }
                 }
-                try {
-                    scanner.close();
+            try {
+                    //scanner.close();
                     is.close();
                     os.close();
                     socket.close();
@@ -66,16 +156,21 @@ public class Client {
             }
 
         }
-    }
 
-    public static void main() throws IOException {
+
+
+    public static void main(String args[]) throws IOException {
         System.setProperty("javax.net.ssl.keyStore", "proj/https_cert/user.p12");
         System.setProperty("javax.net.ssl.keyStorePassword", "changeme");
         System.setProperty("javax.net.ssl.trustStore", "proj/https_cert/usertruststore.jks");
         System.setProperty("javax.net.ssl.trustStorePassword", "changeme");
-        String host = "192.168.0.100";
-        int port = 5000;
-        startClient(host, port);
-
+        if (args.length == 2) {
+            String host = args[0];
+            int port = Integer.parseInt(args[1]);
+            startClient(host, port);
+        } else {
+            System.out.println("Usage: java Client <host> <port>");
+            return;
+        }
     }
 }
