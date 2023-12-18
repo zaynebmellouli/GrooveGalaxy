a49d66ed6e8e9071b2b6160b091135b4e795e061
Cherie000 <93259744+Cherie000@users.noreply.github.com>
2023-12-18 22:03:13 +0000

    heeheh have fun merging

---
+++ src/main/java/proj/server_client/data_objects/SecurityException.java
@@ -0,0 +1,10 @@
+package proj.server_client.data_objects;
+
+public class SecurityException extends Exception{
+        public SecurityException(String message) {
+            super(message);
+        }
+
+        // Optionally, you can add additional constructors, methods, or fields
+
+}
